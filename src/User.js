6fb3ec89007ff29603fb9bfad154e6fd55a24194
prajnarbhat <rbhatprajna@gmail.com>
2025-04-02 16:26:53 +0530

    changes to file

--- src/User.js
+++ src/User.js
@@ -8,12 +8,11 @@ const User = (props) => {
     return (
         <div style={containerStyle} className="container-style">
         <div className="user-style">
-            {arrayOfUserData.length > 0 ? <>
             {arrayOfUserData.map((user,index) => (
                 <Link to={`/users/${user.login}`} style={{ textDecoration: "none" }}>
                 <div key={index}> <UserItem user = {user}/> </div>
                 </Link>
-            ))} </> : <h3> User not found </h3> }
+            ))} 
         </div>
         </div>
     )
